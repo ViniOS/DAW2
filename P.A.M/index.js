d91f477190c6cfe0611b1ddcb8767ffaa9acf4cc
ViniOS <vini.oli.santana@gmail.com>
2024-04-18 19:19:53 -0300

    back

--- P.A.M/index.js
+++ P.A.M/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+
 const { stat } = require('fs')
 const status = require('http-status')
 const app = express()
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
 
 const category = require('./src/routes/category')
 const activity = require('./src/routes/activity')
@@ -10,6 +13,8 @@ const user = require('./src/routes/user')
 app.use('/category', category)
 app.use('/activity', activity)
 app.use('/user', user)
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.get('/', (req, res)=>{
     res.send('Hello World')
--- P.A.M/src/routes/activity.js
+++ P.A.M/src/routes/activity.js
@@ -2,14 +2,27 @@ const { Router } = require('express')
 const router = Router()
 
 router.get('/', (req,res)=>{
+    // #swagger.tags = ['User']
+    // #swagger.description = 'Endpoints para obter atividades.'
+    
     res.send('atividade rodando')
 })
 
 router.post('/', (req,res)=>{
+    // #swagger.tags = ['User']
+    // #swagger.description = 'Endpoint para criar uma atividade.'
+    /* #swagger.responses[201] = { 
+        schema: { $ref: "#/definitions/User" },
+        description: 'atividade criado.' 
+    } */
     res.status(201).send('Creating Activity')
 })
 
 router.put('/:id', (req,res)=>{
+    // #swagger.tags = ['User']
+    // #swagger.description = 'Endpoint para atualizar um usuário.'
+    // #swagger.parameters['id'] = { description: 'ID do usuário.' }
+
     res.status(200).send(`Updating Activity: ${req.params.id}`)
 })
 
